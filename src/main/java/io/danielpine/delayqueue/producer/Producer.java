b72471f735e1105dfe50ddb95054d9d23687c8a9
danielpine <danielpine@sina.com>
2021-12-20 20:19:07 +0800

    Update

--- src/main/java/io/danielpine/delayqueue/producer/Producer.java
+++ src/main/java/io/danielpine/delayqueue/producer/Producer.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.UUID;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/rabbit")
 public class Producer {
 
     @Resource
